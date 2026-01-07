8317883c91ad0b83677e61ed4c02eeccfa01b69f
Miru <miruna.stancu1000@gmail.com>
2026-01-07 21:30:35 +0000

    updated game.c :3

--- game.c
+++ game.c
@@ -2,6 +2,7 @@
 #include "code.h"
 #include "attempt.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_ATTEMPTS 10
 
@@ -30,5 +31,94 @@ game_t* game_create(int mode, player_t *player1, player_t *player2) {
     game->player1 = player1;
     game->player2 = player2;
 
-    game->gamemode = (mode == 0)
+    // set game mode
+    if ( mode == 0 ) {
+        game->gamemode = GAME_PVC;
+    }
+    else {
+        game->gamemode = GAME_PVP;
+    }
+
+    game->attempt_count = 0;
+    game->is_won = false;
+    
+    for ( int i = 0; i < MAX_ATTEMPTS; i ++ )
+        game->attempts[i] = NULL;
+
+    if ( game->gamemode == GAME_PVC ) {
+        game->secret = code_create_random();
+    }
+
+    else {
+        game->secret = code_create_manual();
+    }
+
+    return game;
+}
+
+// Miru :)
+void game_free(game_t* game) {
+
+    if(!game) return; //safetyy - to prevent crash
+
+    // free all attempts
+    for ( int i = 0; i < game->attempt_count; i ++ ) {
+        if ( game->attempts[i]) {
+            attempt_free(game->attempts[i]);
+        }
+    }
+
+    // freeing secret code
+    if(game->secret) {
+        code_free(game->secret);
+    }
+
+    // free game struct
+    free(game);
+
+}
+
+void game_add_attempt(game_t* game, code_t* guess) {
+    if (!game || !guess) return; // safety c heck
+
+    if (game->attempt_count >= MAX_ATTEMPTS) return; // no attempt left
+
+    // create attempt
+    attempt_t* attempt = attempt_create(guess);
+    if (!attempt) return;
+
+    attempt_evaluate(attempt, game->secret);
+
+    // store attempt
+    game->attempts[game->attempt_count] = attempt;
+    game->attempt_count++;
+
+    if (attempt->total_match == CODE_LENGTH) {
+        game->is_won = true;
+    }
+    
+}
+
+// Miru :)
+bool game_is_over(const game_t* game) {
+    if (!game) return true; // safety check :P
+
+    // player guessed code or game is over
+    return game->is_won || game->attempt_count >= MAX_ATTEMPTS;
+
+}
+
+// Miru :)
+bool game_is_won(const game_t* game) {
+    if (!game) return false; // safetyyy - if game doesn't exist it cannot be won
+
+    return game->is_won;
+}
+
+// Miru :)
+int game_attempts_left(const game_t* game) {
+    if (!game) return 0; // safety
+    
+    // total allowed -(minus) how many have been used
+    return MAX_ATTEMPTS - game->attempt_count;
 }
