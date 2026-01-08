b5b4cd6edbadd9d2546c0392979ed7b1919e5d54
sofiyorio <163088570+sofiyorio@users.noreply.github.com>
2026-01-08 12:08:02 +0000

    Added function descriptions

--- attempt.h
+++ attempt.h
@@ -3,16 +3,32 @@
 
 #include "code.h"
 
-// opaque player structure
+// Opaque player structure
 typedef struct attempt_t attempt_t;
 
-// function prototypes
+// Function prototypes
+
+/// @brief Creates a new attempt structure.
+/// @param guess The guess of the attempt to create.
+/// @return A pointer to the newly created attempt.
 attempt_t* attempt_create(code_t* guess);
+
+/// @brief Frees the memory allocated for a given attempt.
+/// @param attempt The attempt to free.
 void attempt_free(attempt_t* attempt);
 
+/// @brief Evaluates if two given attempts have any partial or total matches.
+/// @param attempt The codebreaker attempt.
+/// @param secret The codemaker secret code.
 void attempt_evaluate(attempt_t* attempt, const code_t* secret);
 
-// extra function, unmentioned - getter function
+/// @brief A getter function for an attempt's total match member.
+/// @param player The player which total matches to get.
+/// @return The player's total matches as an int.
 int attempt_get_total_match(const attempt_t* attempt);
+
+/// @brief A getter function for an attempt's partial match member.
+/// @param player The player which partial matches to get.
+/// @return The player's partial matches as an int.
 int attempt_get_partial_match(const attempt_t* attempt);
 #endif // ATTEMPT_H
